c3e9047aa85eda6eb87816c57c7078b71f4570fc
Dahiana <gomezossa0308@gmail.com>
2018-06-30 11:50:54 -0500

    calculadora

---
+++ operaciones.js
@@ -0,0 +1,18 @@
+
+var radio ;
+var altura;
+
+ radio = prompt ('porfavor ingresar el radio');
+ altura = prompt ('porfavor ingresar la altura');
+var volumen_cilindro = parseFloat (Math.PI * radio * radio * altura);
+volumen_cilindro= Math.round (volumen_cilindro);
+
+	console.log ("el volumen del cilindro es " + volumen_cilindro)
+	alert("el volumen del cilindro es " + volumen_cilindro  )
+
+
+var onza=1;
+var tonelada= 0.0000283495;
+
+onza = prompt ('cuantas onzas?');
+var Toneladas = parseFloat ()
