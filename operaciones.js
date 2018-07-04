ff141d210f8529d6a17b983edf6c8299c10a97b3
DahianaGomez <39769072+DahianaGomez@users.noreply.github.com>
2018-07-04 17:24:05 -0500

    calculadora

--- operaciones.js
+++ operaciones.js
@@ -1,33 +1,45 @@
 var operacion;
 var radio ;
 var altura;
-operacion = prompt ('que operacion deseas hacer?');
+operacion = prompt ('1:volumen cilindro 2: convertidor 3:factorizacion');
 
-if (operacion = "volumen cilindro"); {
+if (operacion === "1") {
 
-var radio ;
-var altura;
 
- radio = prompt ('porfavor ingresar el radio');
- altura = prompt ('porfavor ingresar la altura');
-var volumen_cilindro = parseFloat (Math.PI * radio * radio * altura);
-volumen_cilindro= Math.round (volumen_cilindro);
+	radio = prompt ('porfavor ingresar el radio');
+	altura = prompt ('porfavor ingresar la altura');
+	var volumen_cilindro = parseFloat (Math.PI * radio * radio * altura);
+	volumen_cilindro= Math.round (volumen_cilindro);
 
 	console.log ("el volumen del cilindro es " + volumen_cilindro);
 	alert("el volumen del cilindro es " + volumen_cilindro  );
 }
 
-else  if (operacion == "convertidor");
-{
+if(operacion === "2"){
+	
 	var convertidor = prompt ('de onzas a toneladas o toneladas a onzas?');
-tonelada = prompt ('cuantas onzas?');
-var onza;
-var tonelada;
+	if (convertidor === "de toneladas a onzas") {
+		tonelda = prompt ('cuantas toneladas?');
+		var onza;
+		var tonelada;
+		onza = parseFloat (1*35274/tonelada);
+		onza= Math.round (onza);
+		console.log (tonelada + " toneladas, equivalen a " + onza +" onzas");
+		alert (tonelada + " toneladas, equivalen a " + onza +" onzas");
+	}
+	if (convertidor === "de onzas a toneladas") {
+		onza = prompt ('cuantas onzas?');
+	tonelada = parseFloat (1* 0.0000283495 /onza);
+	tonelada = Math.round (tonelada);
+console.log (onza + " onzas, equivalen a " + tonelada +" toneladas");
+		alert (onza + " onzas, equivalen a " + tonelada +" toneladas");
+ }		
+}
 
-
-
-onza = parseFloat (1*35274/tonelada);
-onza= Math.round (onza);
-
-console.log (tonelada + "toneladas, equivalen a " + onza);
+ if (operacion === "3") {
+ 	var numero = prompt ('que numero desea factorizar?');
+ 	var total = 1;
+for (var inicial = 2; total <= numero; inicial++)
+	total *= inicial;
+alert ("el total es " + total);
 }
